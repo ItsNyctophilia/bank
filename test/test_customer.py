31b1af4bf07f56b00deb4ff9324961400dbd75fb
JM Palomares <jm11palomares@gmail.com>
2023-02-24 11:18:34 -0500

    Add unit test for money market module

--- test/test_customer.py
+++ test/test_customer.py
@@ -13,7 +13,7 @@ class TestCustomer(unittest.TestCase):
         self.assertEqual(self.customer.first_name, "John")
         self.assertEqual(self.customer.last_name, "Doe")
         self.assertEqual(self.customer.age, 30)
-        self.assertEqual(self.customer.user_ID, 1)
+        self.assertEqual(self.customer.user_ID, 4)
         self.assertDictEqual(
             self.customer._accounts,
             {
@@ -23,6 +23,24 @@ class TestCustomer(unittest.TestCase):
                 "Money Market Fund": [],
             },
         )
+
+    def test_add_account(self):
+        checking = Checking(1000)
+        self.customer.add_account("Checking", checking)
+        self.assertListEqual(self.customer._accounts["Checking"], [checking])
+
+    def test_deposit_into(self):
+        checking = Checking(1000)
+        self.customer.add_account("Checking", checking)
+        self.customer.deposit_into("Checking", 0, 500)
+        self.assertEqual(checking.balance, 1500)
+
+    def test_get_all_balances(self):
+        savings = Savings(2000)
+        self.customer.add_account("Savings", savings)
+        result = self.customer.get_all_balances()
+        expected = "Savings #1\nAccount balance: $2000.00"
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
