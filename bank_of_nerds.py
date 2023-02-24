14084ec7e73658f0a82c78d7968afa6347a02547
Wesley Smith <Shadowing.bark@gmail.com>
2023-02-24 02:23:31 -0500

    Edit select_users to remove unused variables

--- bank_of_nerds.py
+++ bank_of_nerds.py
@@ -16,7 +16,7 @@ import lib.money_market_fund as market_fund
 
 def create_main_menu():
     """Creates the main menu for the teller program
-    
+
     Returns a tuple of a menu object with the program's allowable
     menu selections, a dictionary with keys and values of the
     menu options, and a reversed dictionary of the same pairings."""
@@ -34,11 +34,11 @@ def create_main_menu():
 
 def get_input(quit_string=None, quit_idx=None):
     """Returns sanitized user input or -1 if program should exit
-    
+
     Keyword arguments:
     quit_string -- word to quit program on
     quit_idx -- numerical index into main menu of the quit button
-    
+
     At least quit_string must be passed as an argument,
     otherwise quit button functionality will not be performed.
     Returns sanitized user input or -1 if the program should
@@ -46,12 +46,12 @@ def get_input(quit_string=None, quit_idx=None):
     while True:
         try:
             selection = input("> ")
-            selection =  selection.title().strip()
+            selection = selection.title().strip()
             if not selection:
                 continue
             if quit_string:
                 if (selection == quit_string or
-                    selection == str(quit_idx)):
+                        selection == str(quit_idx)):
                     return -1
             break
 
@@ -81,43 +81,42 @@ def generate_default_users(users_list):
     users_list.append(user2)
     return users_list
 
+
 def get_users(users_list):
     legend = "(Last), (First) : (User_ID)"
     final_list = [legend, "-" * len(legend)]
     for user in users_list:
-        final_list.append(f"{user.last_name}, {user.first_name} : {user.user_ID}")
+        final_list.append(
+            f"{user.last_name}, {user.first_name} : {user.user_ID}")
     return "\n".join(final_list)
 
-def select_user(default_error,users):
-    """Returns selected user or -1 if invalid option
-    
+
+def select_user(users):
+    """Returns selected user, 0 if invalid option or -1 for "go-back"
+
     Keyword arguments:
-    default_error -- error message string
     user_input -- sanitized output from get_input
     selected_user -- valid user object to be returned 
-    
-    At least default_error, and users must be passed as arguments,
+
+    At least users must be passed as an argument,
     otherwise the function will fail and not select a user.
-    Returns selected user or -1 if the program should produce
+    Returns selected user or 0 if the program should produce
     a value error or if the user selects an id not contained 
-    in the list of users."""
-    print("\n", get_users(users), "\n\n",
-    "Enter a User_ID from the above list: (B for back)", 
-    "\n", sep="")
+    in the list of users. Also will retunr -1 for "go-back"
+    or "quit"."""
     user_input = get_input("B")
     if user_input == -1:
         return -1
     try:
         user_input = int(user_input)
     except ValueError:
-        print("\n", default_error, "\n", sep="")
-        return -1
+        return 0
     if 0 < user_input <= len(users):
         selected_user = users[user_input - 1]
         return selected_user
     else:
-        print("\n", default_error, "\n", sep="")
-        return -1
+        return 0
+
 
 def use_teller():
     """Primary loop for the program"""
@@ -147,10 +146,15 @@ def use_teller():
             print("\n", get_users(users), "\n", sep="")
 
         elif user_input == "Select User":
+            print("\n", get_users(users), "\n\n",
+                  "Enter a User_ID from the above list: (B for back)",
+                  "\n", sep="")
             default_error = "Invalid ID, returning to main menu."
-            return_code = select_user(default_error,users)
+            return_code = select_user(users)
             if return_code == -1:
                 continue
+            if return_code == 0:
+                print("\n", default_error, "\n", sep="")
             selected_user = return_code
 
         elif user_input == "Display Accounts":
@@ -163,7 +167,7 @@ def use_teller():
             account_title = f"{f_name} {l_name}'s Accounts"
             print("\n", account_title, "\n", "-" * len(account_title),
                   "\n", selected_user.get_all_balances(), "\n", sep="")
-            
+
         elif user_input in ("Withdraw", "Deposit"):
             if user_input == "Withdraw":
                 user_func = selected_user.withdraw_from
@@ -178,7 +182,7 @@ def use_teller():
             account_title = f"{f_name} {l_name}'s Accounts"
             print("\n", account_title, "\n", "-" * len(account_title),
                   "\n", selected_user.get_all_balances(), "\n", sep="")
-            print("Select account by 'type:number:amt': (B for back)", 
+            print("Select account by 'type:number:amt': (B for back)",
                   "\n", "ex. '401k:1:$400'", sep="")
             user_input = get_input("B")
             if user_input == -1:
@@ -192,10 +196,11 @@ def use_teller():
                     print("Invalid account number, returning to main menu.")
             elif account_type == "401K":
                 try:
-                    user_func(account_type, int(number) - 1, 
-                                                int(amount), selected_user.age)
+                    user_func(account_type, int(number) - 1,
+                              int(amount), selected_user.age)
                 except (IndexError, TypeError):
                     print("Invalid account number, returning to main menu.")
+
 
 def main():
     use_teller()
@@ -203,7 +208,7 @@ def main():
 
 if __name__ == "__main__":
     # try:
-        main()
+    main()
     # except (Exception, GeneratorExit, KeyboardInterrupt, SystemExit) as e:
-        # name = type(e).__name__
-        # print("Exception of type", name, "prevented program from continuing!")
+    # name = type(e).__name__
+    # print("Exception of type", name, "prevented program from continuing!")
